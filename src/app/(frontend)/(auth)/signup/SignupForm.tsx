b2215ac042cac95af48d8bf0dba50ce220b1c785
Q.Tran <quan.tran@metro.digital>
2025-01-04 13:08:15 +0700

    feat: enhance signup form with validation using react-hook-form

--- src/app/(frontend)/(auth)/signup/SignupForm.tsx
+++ src/app/(frontend)/(auth)/signup/SignupForm.tsx
@@ -5,27 +5,53 @@ import { Label } from '@/components/ui/label';
 import { Button } from '@/components/ui/button';
 import { cn } from '@/utilities/cn';
 import Link from 'next/link';
+import { useForm } from 'react-hook-form';
 
 interface Props {
   className?: string;
 }
 
 const SignupForm: React.FC<Props> = ({ className }) => {
+  const { register, handleSubmit } = useForm();
+
   return (
     <div className={cn('w-[500px] mx-auto p-16 border rounded-2xl', className)}>
       <h1 className="text-xl text-center mb-5">Signup to your account</h1>
 
-      <Label htmlFor="email">Email</Label>
-      <Input id="email" type="email" placeholder="Enter your email" className="mb-4" />
+      <form>
+        <Label htmlFor="email">Email</Label>
+        <Input
+          id="email"
+          type="email"
+          placeholder="Enter your email"
+          className="mb-4"
+          {...register('email', { required: true })}
+        />
 
-      <Label htmlFor="password">Password</Label>
-      <Input id="password" type="password" placeholder="Enter your password" className="mb-4" />
+        <Label htmlFor="password">Password</Label>
+        <Input
+          id="password"
+          type="password"
+          placeholder="Enter your password"
+          className="mb-4"
+          {...register('password', { required: true })}
+        />
 
-      <Link href="/forgotPassword" className="mb-4 text-sm inline-block">
-        Forgot your password?
-      </Link>
+        <Label htmlFor="confirm-password">Confirm Password</Label>
+        <Input
+          id="confirm-password"
+          type="password"
+          placeholder="Confirm your password"
+          className="mb-4"
+          {...register('confirm-password', { required: true })}
+        />
 
-      <Button className="w-full">Login</Button>
+        <Link href="/forgotPassword" className="mb-4 text-sm inline-block">
+          Forgot your password?
+        </Link>
+
+        <Button className="w-full">Login</Button>
+      </form>
     </div>
   );
 };
