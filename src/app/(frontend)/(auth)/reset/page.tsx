7c2389d0741888a31da0505e4451e070cd180955
Q.Tran <quan.tran@metro.digital>
2025-01-06 22:00:07 +0700

    feat: add suspense fallback for ResetPasswordForm and refactor button component

--- src/app/(frontend)/(auth)/reset/page.tsx
+++ src/app/(frontend)/(auth)/reset/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from 'next';
 import ResetPasswordForm from './ResetPasswordForm';
+import { Suspense } from 'react';
 
 export function generateMetadata(): Metadata {
   return {
@@ -11,7 +12,9 @@ const ResetPasswordPage = () => {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
-        <ResetPasswordForm />
+        <Suspense fallback={'...loading'}>
+          <ResetPasswordForm />
+        </Suspense>
       </div>
     </div>
   );
