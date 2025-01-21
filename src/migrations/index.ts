4e108cc445e3afeb9dcffe1c31f6ca6d945416a0
Q.Tran <quan.tran@metro.digital>
2025-01-22 00:59:16 +0700

    feat: add migration index file to manage database migrations

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20250121_174846 from './20250121_174846';
+
+export const migrations = [
+  {
+    up: migration_20250121_174846.up,
+    down: migration_20250121_174846.down,
+    name: '20250121_174846'
+  },
+];
