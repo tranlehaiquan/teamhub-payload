5e02ae25e4d4bb87ebafb99b1ef50e07c4c7dc28
Q.Tran <quan.tran@metro.digital>
2025-02-23 17:11:43 +0700

    feat: add new migration and integrate with payload configuration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20250121_174846 from './20250121_174846';
+import * as migration_20250223_101048 from './20250223_101048';
 
 export const migrations = [
   {
     up: migration_20250121_174846.up,
     down: migration_20250121_174846.down,
     name: '20250121_174846',
+  },
+  {
+    up: migration_20250223_101048.up,
+    down: migration_20250223_101048.down,
+    name: '20250223_101048'
   },
 ];
