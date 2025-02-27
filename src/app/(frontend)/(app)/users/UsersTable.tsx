8336eb09534b5a39a9add3eba053404bf55300c0
Q.Tran <quan.tran@metro.digital>
2025-02-28 00:48:55 +0700

    feat: enhance UsersTable with sorting, filtering, and column visibility features

--- src/app/(frontend)/(app)/users/UsersTable.tsx
+++ src/app/(frontend)/(app)/users/UsersTable.tsx
@@ -1,5 +1,17 @@
 'use client';
-import React from 'react';
+
+import * as React from 'react';
+import {
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from '@tanstack/react-table';
 import {
   Table,
   TableBody,
@@ -8,37 +20,106 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table';
-import { Profile } from '@/payload-types';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 import { api } from '@/trpc/react';
+import { columns } from './columns';
 
 const UsersTable: React.FC = () => {
   const [{ docs: users = [] }] = api.user.getUsers.useSuspenseQuery({});
 
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([]);
+  const [columnVisibility, setColumnVisibility] = React.useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = React.useState({});
+  const [globalFilter, setGlobalFilter] = React.useState('');
+
+  const table = useReactTable({
+    data: users,
+    columns,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+      globalFilter,
+    },
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    onGlobalFilterChange: setGlobalFilter,
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    debugTable: true,
+  });
+
   return (
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[100px]">ID</TableHead>
-          <TableHead>First Name</TableHead>
-          <TableHead>Last Name</TableHead>
-          <TableHead>Roles</TableHead>
-          <TableHead>Email</TableHead>
-          <TableHead>Verify email</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {users.map((user) => (
-          <TableRow key={user.id}>
-            <TableCell className="font-medium">{user.id}</TableCell>
-            <TableCell>{(user.profile as Profile)?.firstName}</TableCell>
-            <TableCell>{(user.profile as Profile)?.lastName}</TableCell>
-            <TableCell>{user.roles?.join(', ')}</TableCell>
-            <TableCell>{user.email}</TableCell>
-            <TableCell>{user._verified && 'Yes'}</TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+    <div>
+      <div className="flex items-center py-4">
+        <Input
+          placeholder="Search all columns..."
+          value={globalFilter ?? ''}
+          onChange={(event) => setGlobalFilter(event.target.value)}
+          className="max-w-sm"
+        />
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="ml-auto">
+              Columns
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            {table.getAllColumns().map((column) => {
+              return (
+                <DropdownMenuCheckboxItem
+                  key={column.id}
+                  className="capitalize"
+                  checked={column.getIsVisible()}
+                  onCheckedChange={(value) => column.toggleVisibility(!!value)}
+                >
+                  {column.id}
+                </DropdownMenuCheckboxItem>
+              );
+            })}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => (
+                <TableHead key={header.id} colSpan={header.colSpan}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(header.column.columnDef.header, header.getContext())}
+                </TableHead>
+              ))}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows.map((row) => (
+            <TableRow key={row.id}>
+              {row.getVisibleCells().map((cell) => (
+                <TableCell key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
   );
 };
 
---
+++ src/app/(frontend)/(app)/users/columns.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { ColumnDef } from '@tanstack/react-table';
+import { Profile, User } from '@/payload-types';
+
+export const columns: ColumnDef<User>[] = [
+  {
+    accessorKey: 'id',
+    header: 'ID',
+    enableGlobalFilter: false, // exclude from global search
+  },
+  {
+    accessorKey: 'firstName',
+    accessorFn: (row) => (row.profile as Profile)?.firstName,
+    header: 'First Name',
+  },
+  {
+    accessorKey: 'lastName',
+    accessorFn: (row) => (row.profile as Profile)?.lastName,
+    header: 'Last Name',
+  },
+  {
+    accessorKey: 'roles',
+    header: 'Roles',
+    cell: ({ row }) => row.original.roles?.join(', '),
+  },
+  {
+    accessorKey: 'email',
+    header: 'Email',
+  },
+  {
+    accessorKey: '_verified',
+    header: 'Verify email',
+    cell: ({ row }) => (row.original._verified ? 'Yes' : 'No'),
+    enableGlobalFilter: false, // exclude from global search
+  },
+];
